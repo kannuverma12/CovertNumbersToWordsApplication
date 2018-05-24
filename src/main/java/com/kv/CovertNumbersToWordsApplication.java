207642104cc88453aa21e934e8e5eeab7659f335
Karan Verma <karan.verma@kv.local>
2018-05-24 11:31:40 +0530

    create repo

---
+++ src/main/java/com/kv/CovertNumbersToWordsApplication.java
@@ -0,0 +1,12 @@
+package com.kv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovertNumbersToWordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovertNumbersToWordsApplication.class, args);
+	}
+}
